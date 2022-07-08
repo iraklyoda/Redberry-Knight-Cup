941f28f187f4aea9f340c74f455ccac2980bd020
iraklyoda <iraklyoda42@gmail.com>
2022-07-08 04:13:26 +0400

    split form between two pages

--- script.js
+++ script.js
@@ -11,31 +11,8 @@ const form = document.querySelector("#create-account-form");
 const errorsElement = document.querySelector("#errors");
 const firstNextBtn = document.querySelector("#first-next-btn");
 const secondStepBack = document.querySelector("#second-step-back-btn");
-const four = document.querySelector("h4.first-step");
-const fourSecond = document.querySelector("h4.second-step");
-const firstSteps = document.querySelectorAll(".first-step");
-const secondSteps = document.querySelectorAll(".second-step");
-const levels = document.getElementsByName('category');
 
-if(sessionStorage.step === "2"){
-  changeSteps();
-}
 console.log(sessionStorage);
-
-secondSquare.addEventListener('click', () => {
-  console.log(document.querySelector('.player-level').value);
-})
-
-
-// document.querySelector('.player-level').addEventListener('click', () => {
-//   if(document.querySelector('.player-level').value != ""){
-//   document.querySelector('.level-placeholder').classList.add('display-n');
-//   }
-// })
-
-
-
-
 
 // variables
 let errors = {};
@@ -47,14 +24,17 @@ inputs.forEach((input) => {
     sessionStorage.setItem("active", "firstTrue");
   });
 });
+
 if (sessionStorage.active == "secondTrue") {
   firstSquare.querySelector("span").classList.add("display-n");
   firstSquare.querySelector("img").classList.add("display-y");
+  inputs.forEach(input => {
+    input.parentElement.classList.add('success');
+  });
   firstSquare.classList.add("greenColor");
 } else if (sessionStorage.active == "firstTrue") {
   firstSquare.classList.add("greenColor");
 }
-
 
 // grab inputs from session storage
 if (sessionStorage.name) {
@@ -70,16 +50,14 @@ if (sessionStorage.date) {
   dateInput.value = sessionStorage.date;
 }
 
-
-
 // functions
 
-function checkFirstPage() {
+form.addEventListener("submit", (event) => {
   localStorage.setItem("name", nameInput.value);
   localStorage.setItem("email", emailInput.value);
   localStorage.setItem("number", numberInput.value);
   localStorage.setItem("date", dateInput.value);
-  validateFormFirst();
+  validateForm();
   const errorElementRemoveBtn = document.querySelectorAll(".error-delete");
   errorElementRemoveBtn.forEach((removeBtn) => {
     removeBtn.addEventListener("click", () => {
@@ -91,37 +69,14 @@ function checkFirstPage() {
     firstSquare.querySelector("span").classList.add("display-n");
     firstSquare.querySelector("img").classList.add("display-y");
     sessionStorage.setItem("active", "secondTrue");
-    changeSteps();
-    } else {
+    form.submit();
+  } else {
+    event.preventDefault();
     sessionStorage.setItem("active", "firstTrue");
     firstSquare.querySelector("img").classList.remove("display-y");
     firstSquare.querySelector("span").classList.remove("display-n");
   }
-}
-
-function changeSteps() {
-  firstSteps.forEach((firstStep) => {
-    firstStep.classList.add("display-n");
-  });
-  secondSteps.forEach((secondStep) => {
-    secondStep.classList.remove("display-n");
-  });
-  secondSquare.classList.remove('bolded');
-  sessionStorage.setItem('step', '2');
-
-}
-
-firstNextBtn.addEventListener("click", () => {
-  checkFirstPage();
-});
-
-document.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    checkFirstPage();
-  }
 })
-
-
 
 function isFormValid() {
   const inputContainers = document.querySelectorAll(".input-group");
@@ -134,7 +89,7 @@ function isFormValid() {
   return valid;
 }
 
-function validateFormFirst() {
+function validateForm() {
   errors = [];
   errorsElement.innerHTML = ``;
   // Name
@@ -178,11 +133,6 @@ function validateFormFirst() {
     setSuccess(dateInput);
   }
 
-  // Categories
-  if (!checkCategories()){
-    setError(document.querySelector(".selected-level"), "Invalid category", "Please enter valid Category");
-  }
-
   sessionStorage.setItem("date", dateInput.value);
   console.log(errors);
   for (const [reason, message] of Object.entries(errors)) {
@@ -199,19 +149,6 @@ function validateFormFirst() {
   }
 }
 
-
-
-function checkCategories() {
-  checked = false;
-  levels.forEach(level => {
-    if(level.checked){
-      checked = true;
-    }
-  })
-  return checked;
-}
-
-
 function setError(element, errorReason, errorMessage) {
   const parent = element.parentElement;
   parent.classList.add("error");
@@ -224,6 +161,7 @@ function setSuccess(element) {
   parent.classList.remove("error");
   parent.classList.add("success");
 }
+
 function validateEmail(email) {
   let re = /@redberry.ge\s*$/i;
   let reTwo =
@@ -234,62 +172,3 @@ function validateEmail(email) {
 // Between two steps
 
 
-secondStepBack.addEventListener("click", () => {
-  firstSteps.forEach((firstStep) => {
-    firstStep.classList.remove("display-n");
-  });
-  secondSteps.forEach((secondStep) => {
-    secondStep.classList.add("display-n");
-  });
-  sessionStorage.setItem('step', '1');
-});
-
-
-// Custom Select elements
-
-// For Levels
-const selectedLevel = document.querySelector(".selected-level");
-const optionsContainerLevel = document.querySelector(".options-container-level");
-const optionsListLevel = document.querySelectorAll('.option-level');
-
-  selectedLevel.addEventListener('click', () => {
-    console.log(checkCategories());
-    optionsContainerLevel.classList.toggle('active');
-  })
-
-
-optionsListLevel.forEach( option => {
-  option.addEventListener('click', () => {
-    selectedLevel.innerHTML = option.querySelector("label").innerHTML;
-    option.querySelector('input').checked = true;
-    optionsContainerLevel.classList.remove("active");
-  })
-});
-
-// For characters
-const selectedCharacter = document.querySelector(".selected-character");
-const optionsContainerCharacter = document.querySelector(".options-container-character");
-const optionsListCharacter = document.querySelectorAll('.option-character');
-
-  selectedCharacter.addEventListener('click', () => {
-    optionsContainerCharacter.classList.toggle('active');
-  })
-
-
-optionsListCharacter.forEach( option => {
-  option.addEventListener('click', () => {
-    selectedCharacter.innerHTML = option.querySelector("label").innerHTML;
-    option.querySelector('input').checked = true;
-    optionsContainerCharacter.classList.remove("active");
-  })
-});
-
-function validateSecond(){
-  errors = [];
-  errorsElement.innerHTML = ``;
-
-}
-
-
-
-console.log(levels.value);
---
+++ script2.js
@@ -0,0 +1,163 @@
+let params = new URLSearchParams(location.search);
+
+if(!params.get('name')){
+  window.location.href = "./form.html";
+}
+
+// Const variables
+const inputs = document.querySelectorAll(".input-group");
+const secondSquare = document.querySelector(".second");
+
+const errorsElement = document.querySelector("#errors");
+const form = document.querySelector("#create-account-form");
+const levels = document.getElementsByName("category");
+const characters = document.getElementsByName("character");
+const championships = document.getElementsByName("championship");
+const selectedLevel = document.querySelector(".selected-level");
+const radioInput = document.querySelector(".radio-input");
+const optionsContainerLevel = document.querySelector(
+  ".options-container-level"
+);
+
+const selectedCharacter = document.querySelector(".selected-character");
+const optionsContainerCharacter = document.querySelector(
+  ".options-container-character"
+);
+const optionsListCharacter = document.querySelectorAll(".option-character");
+const optionsListLevel = document.querySelectorAll(".option-level");
+
+let errors = {};
+
+if(sessionStorage.active == "firstTrue"){
+  secondSquare.classList.add("greenColor");
+
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    secondSquare.classList.add("greenColor");
+    sessionStorage.setItem("active", "firstTrue");
+  });
+});
+
+// # running on https://www.example.com?name=n1&name=n2
+console.log(params.get('name'));
+
+
+
+
+
+form.addEventListener("submit", (event) => {
+  validateForm();
+  const errorElementRemoveBtn = document.querySelectorAll(".error-delete");
+  errorElementRemoveBtn.forEach((removeBtn) => {
+    removeBtn.addEventListener("click", () => {
+      console.log("cekva");
+      removeBtn.parentElement.classList.add("display-n");
+    });
+  });
+  if (isFormValid()) {
+    form.submit();
+  } else {
+    event.preventDefault();
+  }
+})
+
+function validateForm() {
+  errors = [];
+  errorsElement.innerHTML = ``;
+  // Categories
+  if (!checkRadios(levels)) {
+    setError(selectedLevel, "categories", "Pls choose category");
+  } else {
+    setSuccess(selectedLevel);
+  }
+
+  if(!checkRadios(characters)) {
+    setError(selectedCharacter, "characters", "Pls choose character");
+  } else {
+    setSuccess(selectedCharacter);
+  }
+
+  if(!checkRadios(championships)) {
+    setError(radioInput, "yes-or-no", "pls choose yes or no");
+  } else {
+    setSuccess(radioInput);
+  }
+
+  for (const [reason, message] of Object.entries(errors)) {
+    console.log(`${reason}, ${message}`);
+    errorsElement.innerHTML += `
+    <div class="err">
+    <img src="./media/second_page/Vectoreroor.svg" class="error-svg">
+    <p class="invalid">${reason}</p>
+    <img src="./media/second_page/Vectordelete.svg" class="error-delete">
+    <div class="error-stroke"></div>
+    <p class="error-message">${message}</p>
+  </div>
+    `;
+  }
+}
+
+function isFormValid() {
+  const inputContainers = document.querySelectorAll(".input-group");
+  let valid = true;
+  inputContainers.forEach((container) => {
+    if (container.classList.contains("error")) {
+      valid = false;
+    }
+  });
+  return valid;
+}
+
+function checkRadios(radios) {
+  checked = false;
+  radios.forEach((radio) => {
+    if (radio.checked) {
+      checked = true;
+    }
+  });
+  return checked;
+}
+
+
+// Custom selects
+
+selectedLevel.addEventListener("click", () => {
+  optionsContainerLevel.classList.toggle("active");
+});
+
+optionsListLevel.forEach((option) => {
+  option.addEventListener("click", () => {
+    selectedLevel.innerHTML = option.querySelector("label").innerHTML;
+    option.querySelector("input").checked = true;
+    optionsContainerLevel.classList.remove("active");
+  });
+});
+
+// For characters
+
+selectedCharacter.addEventListener("click", () => {
+  optionsContainerCharacter.classList.toggle("active");
+});
+
+optionsListCharacter.forEach((option) => {
+  option.addEventListener("click", () => {
+    selectedCharacter.innerHTML = option.querySelector("label").innerHTML;
+    option.querySelector("input").checked = true;
+    optionsContainerCharacter.classList.remove("active");
+  });
+});
+
+function setError(element, errorReason, errorMessage) {
+  const parent = element.parentElement;
+  parent.classList.add("error");
+  parent.classList.remove("success");
+  errors[errorReason] = errorMessage;
+}
+
+function setSuccess(element) {
+  const parent = element.parentElement;
+  parent.classList.remove("error");
+  parent.classList.add("success");
+}
